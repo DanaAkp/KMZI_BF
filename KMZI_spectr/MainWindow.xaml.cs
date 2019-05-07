bbb80eb64fd1db3cac5c2eeb2c7aa2677c6ca496
Danagul Akpaeva <aidanagul10@gmail.com>
2019-05-08 01:25:38 +0300

    Спектры и вес готовы. Сделать нелинейность и другие свойства

--- KMZI_spectr/MainWindow.xaml.cs
+++ KMZI_spectr/MainWindow.xaml.cs
@@ -31,60 +31,79 @@ namespace KMZI_spectr
             try
             {
                 int[] vec = GetVector(tbFunc.Text.Trim());
-                if (!CheckVector(vec)) throw new Exception("Error");
-                int[] spectr = new int[vec.Length];
+                if (!CheckVector(vec)) throw new Exception("Ошибка");
+                tblRes.Text = "Вес равен " + weight(vec)+"\n";
+                int[] Furie = spectrF(vec);
+                int[] WA = spectrWA(vec);
 
-                for (int i = 0; i < vec.Length; i++)
-                {
-                    int[] x = getBinaryCode(i);
-                    for(int j = 0; j < vec.Length; j++)
-                    {
-                        int[] u = getBinaryCode(j);
-                        int buf = 0;
-                        for (int k = 0; k < NumberOfVariables; k++)
-                        {
-                            buf += x[k] * u[k];
-                            buf %= 2;
-                        }
-
-                        spectr[i] += (int)Math.Pow(-1, buf) * vec[i];
-                    }
-                    
-                }
-
-                tblRes.Text = "";
-                for (int i = 0; i < spectr.Length; i++) tblRes.Text += spectr[i] + " ";
+                for (int i = 0; i < Furie.Length; i++)
+                    tblRes.Text += Furie[i] + " ";
+                tblRes.Text += "\n";
+                for (int i = 0; i < Furie.Length; i++)
+                    tblRes.Text += WA[i] + " ";
             }
             catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private int[] spectrF(int[] vec)
+        {
+            int[] spectr = new int[vec.Length];
+            for (int i = 0; i < vec.Length; i++)
+            {
+                int[] u = getBinaryCode(i);
+                for (int j = 0; j < vec.Length; j++)
+                {
+                    int[] x = getBinaryCode(j);
+                    int buf = 0;
+                    for (int k = 0; k < x.Length; k++)
+                    {
+                        if (x[k] == 1 && u[k] == 1) buf++;
+                    }
+                    buf %= 2;
+                    buf = (int)Math.Pow(-1, buf);
+                    buf *= vec[j];
+                    spectr[i] += buf;
+                }
+            }
+            return spectr;
+        }
+        private int[] spectrWA(int[] vec)
+        {
+            int[] spectr = new int[vec.Length];
+            for (int i = 0; i < vec.Length; i++)
+            {
+                int[] u = getBinaryCode(i);
+                for (int j = 0; j < vec.Length; j++)
+                {
+                    int[] x = getBinaryCode(j);
+                    int buf = 0;
+                    for (int k = 0; k < x.Length; k++)
+                    {
+                        if (x[k] == 1 && u[k] == 1) buf++;
+                    }
+                    buf += vec[j];
+                    buf %= 2;
+                    buf = (int)Math.Pow(-1, buf);
+                    spectr[i] += buf;
+                }
+            }
+            return spectr;
         }
         private int[] getBinaryCode(int j)
         {
             int[] num=new int[NumberOfVariables];
             string Binarycode = Convert.ToString(j, 2);
-            for (int i = 0; i < Binarycode.Length; i++)
-                num[NumberOfVariables-i-1] = int.Parse(Binarycode[i].ToString());
+
+            for (int i = Binarycode.Length-1; i >=0; i--)
+                num[Binarycode.Length -1- i] = int.Parse(Binarycode[i].ToString());
+            
+                Array.Reverse(num);
             return num;
         }
-        //private int x_u(int[] x, int[] u)
-        //{
-        //    int x_u = 0;
-        //    for (int i = 0; i < x.Length; i++)
-        //    {
-        //        x_u += x[i] * u[i];
-        //        x_u %= 2;
-        //    }
-        //    return x_u;
-        //}
         private int weight(int[] vec)
         {
             int c = 0;
             foreach (int x in vec) if (x == 1) c++;
             return c;
-        }
-        private void BtnWolsh_Adamar_Click(object sender, RoutedEventArgs e)
-        {
-            int[] vec = GetVector(tbFunc.Text.Trim());
-
         }
         private int[] GetVector(string s)
         {
